4a43a7097f84b025c33c46a8001c57ac7626ee50
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-04-26 22:17:40 +0530

    Create Operation.java

---
+++ Basics/Operation.java
@@ -0,0 +1,15 @@
+public class Operation{
+	public static void main(String args[]) {
+		int a, b;		//declaring the variable of Integer data type
+		a=3;	b=5;	//assigning some value with the variables
+		System.out.println("Addition Value is = " + (a+b));	//the '+' symbol means concatenation in print method, inside bracket it will act as a plus operator
+		
+		//also you can store the addition value in another variable
+		int c;
+		c= a+b;			//this will store the addition value in c
+		System.out.println("The Addition Value in C = " + c);
+	}
+}
+
+
+//Same thing you can do with other operation like subtraction, multiplication, division etc...
