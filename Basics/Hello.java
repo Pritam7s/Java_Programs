6b04eb9958dec701a1b8cd402dd1b0bc2b2e00eb
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-04-26 21:49:18 +0530

    Create Hello.java

---
+++ Basics/Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+	public static void main(String args[]) {
+		System.out.print("Hello There");    //for printing any statement (cursor remains on the same line)
+	    System.out.println("Hey, Supp!");   //this is used generally, when we need a next-line to print another statement after this line (moves the cursor to next line)
+	}
+}
