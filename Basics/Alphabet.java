436f82bf9d06bd7a61552a01e2680626f4aceffe
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-04-29 23:18:22 +0530

    Create Alphabet.java

---
+++ Basics/Alphabet.java
@@ -0,0 +1,17 @@
+//Program to check a Character is alphabet or not?
+
+import java.util.Scanner;
+public class Alphabet {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c;
+		System.out.println("Enter a character: ");
+		c=sc.next().charAt(0);
+		if(c>='a' && c<='z'|| c>='A' && c<='Z'){
+			System.out.println("Its an Alphabet.");
+		}
+		else {
+			System.out.println("Its not an Alphabet");
+		}
+	}
+}
