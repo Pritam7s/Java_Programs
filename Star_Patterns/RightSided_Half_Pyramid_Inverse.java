75564d981630add3587522c3ba689e1fe73d3ad1
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-04-30 23:20:28 +0530

    Update and rename RightSidedHalfPyramidInverse.java to RightSided_Half_Pyramid_Inverse.java

--- Star_Patterns/RightSided_Half_Pyramid_Inverse.java
+++ Star_Patterns/RightSided_Half_Pyramid_Inverse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class RightSidedHalfPyramidInverse {
+public class RightSided_Half_Pyramid_Inverse {
 	public static void main(String[] args) {
 		
          Scanner sc = new Scanner(System.in);
