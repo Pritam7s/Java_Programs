f82ae17e41f1e697cd521e85cc1b557a3ee1f205
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-04-30 23:17:02 +0530

    Create RightSidedHalfPyramidInverse.java

---
+++ Star_Patterns/RightSidedHalfPyramidInverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class RightSidedHalfPyramidInverse {
+	public static void main(String[] args) {
+		
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows needed to print the pattern ");
+        int rows = sc.nextInt();		// taking the number of rows from the user
+        System.out.println(); 			// just an extra next line
+        
+        for(int i=1; i<=rows; i++) {	// this will iterate until the number of total rows
+            for (int j=rows; j>=i; j--) {	// for printing star in decreasing order
+                System.out.print("*");
+            }
+            System.out.println();		//this will move the cursor to the next row and the star will print there
+        }
+	}
+}
+
